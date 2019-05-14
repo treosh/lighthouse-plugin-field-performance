5f1a1b2d12cb6637bab43325833353720276a6c0
Artem Denysov <denysov.artem@gmail.com>
2019-05-14 17:37:02 +0300

    Adds test

---
+++ test/test-run.js
@@ -0,0 +1,14 @@
+const { exec } = require('child_process')
+const data = require('./mock/load-experiance')
+
+// @todo mock getCruxData to return data for testing purposes
+
+;(() => {
+  exec('npx lighthouse https://www.apple.com --plugins=lighthouse-plugin-field-performance --view --chrome-flags="--headless" --output-path=./results/apple.html', {
+    cwd: '../'
+  }, (err, stdout, stderr) => {
+    if (err) console.log(err)
+    console.log(stdout)
+  })
+})()
+
