9c347dbb8921ba7a7f36bace8677658ad3c45681
Artem Denysov <denysov.artem@gmail.com>
2019-05-23 01:27:25 +0300

    Fixes mock run

--- script/utils.js
+++ script/utils.js
@@ -1,13 +1,13 @@
 const { resolve } = require('path')
 
 exports.stubPSI = data => {
-  const resolved = resolve(__dirname, '../lighthouse-plugin-field-performance/psi.js')
+  const resolved = resolve(__dirname, '../lighthouse-plugin-field-performance/utils/run-psi.js')
   require.cache[resolved] = {
     id: resolved,
     filename: resolved,
     loaded: true,
     exports: {
-      getCruxData: () => {
+      runPsi: () => {
         return data
       }
     }
